aaf8a72bec0b408884098c5fe489c6ac36d4e0b8
atasa <atasa@naver.com>
2021-09-02 16:52:09 +0900

    first commit

---
+++ src/test/java/com/cos/blog/BlogApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cos.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
+
+
+
+
+
